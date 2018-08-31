deef6c8064b6e8941725775c3d0c33d86fe1b4ce
Bartek Karasek <bartlomiej.karasek@gmail.com>
2018-08-31 09:18:10 +0200

    test

---
+++ 1/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cout << "a lot of stuff should be here" << std :: endl;
+
+
+  int uselessVariable;
+
+
+  if(true) {
+    // TODO
+  }
+
+}
